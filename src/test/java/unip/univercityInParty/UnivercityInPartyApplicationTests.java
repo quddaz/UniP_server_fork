80daa0d771a3d30e337802239f49fc5fb21c5ea1
김병웅 <quddnddl35@naver.com>
2024-09-11 14:37:27 +0900

    init

---
+++ src/test/java/unip/univercityInParty/UnivercityInPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package unip.univercityInParty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivercityInPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
